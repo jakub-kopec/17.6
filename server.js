65437b1985a11471701fca3bdafe73ca89a5ca07
jakub-kopec <agh.jakub@gmail.com>
2019-06-30 20:50:03 +0200

    17.6 ready

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'pug')
+app.set('views','./views')
+
+app.get('/', function (req, res) {
+    res.render('auth-google')
+})
+
+app.get('/auth/google', function(req, res){
+    var userName = req.query.username
+    res.render('logged-in', {
+        username: userName
+    })
+})
+
+app.listen(3000)
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+})
